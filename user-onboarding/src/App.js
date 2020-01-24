0cc6692b0bba6f2db82fe381643c00f8e236ac9d
Francois Chery <swa_isthecreator@Francoiss-MBP.home>
2020-01-24 13:24:43 -0500

    designed form

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -5,7 +5,7 @@ import UserForm from './component/UserForm';
 function App() {
   return (
     <div className="App">
-      <h1>On Boarding Process</h1>
+      <h1>Welcome to our on Boarding Process</h1>
       <UserForm />
     </div>
   );
--- user-onboarding/src/component/UserForm.js
+++ user-onboarding/src/component/UserForm.js
@@ -1,21 +1,84 @@
 import React, {useState, useEffect} from 'react'
 import { withFormik, Form, Field } from 'formik';
+import {Button} from 'reactstrap';
 import axios from 'axios';
 
 function UserForm() {
     return (
         <div className='container'>
             <Form>
-                <label htmlFor='name'>
-                    Enter your name
+                <h2>Name</h2>
+                <label htmlFor='firstname'>
+                    First *
 
                     <Field 
-                    id="name"
+                    className='input'
+                    id="firstname"
                     type="text"
-                    name="name"
-                    placeholder="name"
+                    name="firstname"
+                    placeholder='first name'
                     />
                 </label>
+                <label htmlFor='middlename'>
+                    Middle
+
+                    <Field 
+                    className='input'
+                    id="middlename"
+                    type="text"
+                    name="middlename"
+                    placeholder="middle name"
+                    />
+                </label>
+                <label htmlFor='lastname'>
+                    Last *
+
+                    <Field 
+                    className='input'
+                    id="lastname"
+                    type="text"
+                    name="lastname"
+                    placeholder="last name"
+                    />
+                </label>
+                <label className='email' htmlFor='email'>
+                    Email *
+
+                    <Field 
+                    className='input'
+                    id="email"
+                    type="text"
+                    name="email"
+                    placeholder="example@email.com"
+                    />
+                </label>
+                <h2>Password</h2>
+                <label className='password' htmlFor='password'>
+                    Create a password *
+
+                    <Field 
+                    className='input'
+                    id="password"
+                    type="text"
+                    name="password"
+                    placeholder='enter password'
+                    />
+                </label>
+                <h2>Terms Of Agreement</h2>
+                <p>You must adhere to all terms on this form.</p>
+                <label className='agreement' htmlFor='agreement'>
+                    Terms of Service
+
+                    <Field 
+                    className='check-box'
+                    id="agreement"
+                    type="checkbox"
+                    name="agreement"
+                    />
+                </label>
+                <div className='btn'>
+                    <Button>Submit</Button>
+                </div>
             </Form>
         </div>
     )
